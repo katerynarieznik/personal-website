142b4b99b688d7f20df692002c4303fd909f9e40
Kateryna Rieznik <rieznik.k@gmail.com>
2024-03-15 15:56:23 +0200

    setup fonts

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,13 +1,65 @@
-import localFont from "next/font/local";
+import localFont from 'next/font/local'
 
 export const fontSans = localFont({
-  src: "../fonts/WorkSansGX.ttf",
-  display: "swap",
-  variable: "--font-sans",
-});
+  src: [
+    {
+      path: '../styles/fonts/CraftworkGrotesk-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../styles/fonts/CraftworkGrotesk-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../styles/fonts/CraftworkGrotesk-SemiBold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../styles/fonts/WorkSans-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../styles/fonts/CraftworkGrotesk-Heavy.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+  variable: '--font-sans',
+})
 
 export const fontSerif = localFont({
-  src: "../fonts/CraftworkGroteskGX.ttf",
-  display: "swap",
-  variable: "--font-serif",
-});
+  src: [
+    {
+      path: '../styles/fonts/WorkSans-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../styles/fonts/WorkSans-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../styles/fonts/WorkSans-Semibold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../styles/fonts/WorkSans-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../styles/fonts/WorkSans-Black.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+  variable: '--font-sans',
+})
