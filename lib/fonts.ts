86d8685962a9ac14b605e62b8db69d49b6ec4e27
Kateryna Rieznik <rieznik.k@gmail.com>
2024-04-23 12:41:23 +0300

    move fonts to assets

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -3,27 +3,27 @@ import localFont from 'next/font/local'
 export const fontSans = localFont({
   src: [
     {
-      path: '../styles/fonts/CraftworkGrotesk-Regular.woff2',
+      path: '../assets/fonts/CraftworkGrotesk-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../styles/fonts/CraftworkGrotesk-Medium.woff2',
+      path: '../assets/fonts/CraftworkGrotesk-Medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../styles/fonts/CraftworkGrotesk-SemiBold.woff2',
+      path: '../assets/fonts/CraftworkGrotesk-SemiBold.woff2',
       weight: '600',
       style: 'normal',
     },
     {
-      path: '../styles/fonts/WorkSans-Bold.woff2',
+      path: '../assets/fonts/WorkSans-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
     {
-      path: '../styles/fonts/CraftworkGrotesk-Heavy.woff2',
+      path: '../assets/fonts/CraftworkGrotesk-Heavy.woff2',
       weight: '900',
       style: 'normal',
     },
@@ -35,27 +35,27 @@ export const fontSans = localFont({
 export const fontSerif = localFont({
   src: [
     {
-      path: '../styles/fonts/WorkSans-Regular.woff2',
+      path: '../assets/fonts/WorkSans-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../styles/fonts/WorkSans-Medium.woff2',
+      path: '../assets/fonts/WorkSans-Medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../styles/fonts/WorkSans-Semibold.woff2',
+      path: '../assets/fonts/WorkSans-Semibold.woff2',
       weight: '600',
       style: 'normal',
     },
     {
-      path: '../styles/fonts/WorkSans-Bold.woff2',
+      path: '../assets/fonts/WorkSans-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
     {
-      path: '../styles/fonts/WorkSans-Black.woff2',
+      path: '../assets/fonts/WorkSans-Black.woff2',
       weight: '900',
       style: 'normal',
     },
